b07e8c0853e753ea8110ca2b9cdbd68f1ad10a1a
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-08-21 04:02:30 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,5 +3,9 @@ pigeon_hash = {}
   data.each_pair do |attribute, values|
     values.each_pair do |specific_values, pigeons|
       pigeons.each do |pigeon|
+                if pigeon_hash[pigeon].nil?
+          # binding.pry
+          pigeon_hash[pigeon] = {} 
+        end
         
 end
