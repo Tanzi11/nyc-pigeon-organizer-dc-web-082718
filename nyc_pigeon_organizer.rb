7159c2868a4ac2ff039b105e408499227d450e8c
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-08-21 04:02:15 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,5 +2,6 @@ def nyc_pigeon_organizer(data)
 pigeon_hash = {}
   data.each_pair do |attribute, values|
     values.each_pair do |specific_values, pigeons|
-      
+      pigeons.each do |pigeon|
+        
 end
