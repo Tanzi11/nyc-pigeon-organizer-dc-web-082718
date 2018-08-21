a3ec30733cd02e522c8e288a7ad80a2733dcc673
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-08-21 04:10:28 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attribute_category, attribute_values|
+    attribute_values.each do |attribute_value, names|
+      names.each do |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][attribute_category] ||= []
+        pigeon_list[name][attribute_category] << attribute_value.to_s
+      end
+    end
+  end
+  pigeon_list
 end
+
