2e95f5a64fbf5cfc4773ae3a654c3423abcc57e1
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-08-21 04:03:26 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,21 +1,2 @@
 def nyc_pigeon_organizer(data)
-pigeon_hash = {}
-  data.each_pair do |attribute, values|
-    values.each_pair do |specific_values, pigeons|
-      pigeons.each do |pigeon|
-                if pigeon_hash[pigeon].nil?
-          pigeon_hash[pigeon] = {} 
-        end
-          if pigeon_hash[pigeon][attribute]
-          pigeon_hash[pigeon][attribute] << specific_values.to_s
-        else
-          pigeon_hash[pigeon][attribute] = []
-          pigeon_hash[pigeon][attribute] << specific_values.to_s
-        end
-          if pigeon_hash[pigeon][attribute]
-          pigeon_hash[pigeon][attribute] << specific_values.to_s
-        else
-          pigeon_hash[pigeon][attribute] = []
-          pigeon_hash[pigeon][attribute] << specific_values.to_s
-        end
 end
